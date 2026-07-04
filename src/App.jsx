3098f98f07498ca824ca4d0f924731e1d91cff8f
Figo Kosasih <nnah615@gmail.com>
2026-07-04 09:00:18 +0700

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
+import Navbar from './components/navbar';
 import Dashboard from './pages/Dashboard';
 import InputLog from './pages/InputLog';
 import Medication from './pages/Medication';
